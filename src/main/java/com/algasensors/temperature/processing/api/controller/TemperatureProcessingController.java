4fccf7c2120536cc3922736109ba31d4d7bb415f
Henio Junior <henioajunior@gmail.com>
2026-03-12 15:48:47 -0300

    feat: add custom RabbitMQ message headers for sensor ID in

--- src/main/java/com/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
+++ src/main/java/com/algasensors/temperature/processing/api/controller/TemperatureProcessingController.java
@@ -4,6 +4,7 @@ import com.algasensors.temperature.processing.api.model.TemperatureLogOutput;
 import io.hypersistence.tsid.TSID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -49,7 +50,11 @@ public class TemperatureProcessingController {
         String routingKey = "";
         Object payload = logoutput;
 
-        rabbitTemplate.convertAndSend(exchange, routingKey, payload);
+        MessagePostProcessor messagePostProcessor =  message -> {
+            message.getMessageProperties().setHeader("sensorId", logoutput.getSensorId().toString());
+            return message;
+        };
+        rabbitTemplate.convertAndSend(exchange, routingKey, payload, messagePostProcessor);
 
     }
 }
